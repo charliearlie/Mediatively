5f82d8987ccb5b60cee17e699cab38acb4796049
Charlie Waite <charlie@Charlies-MacBook-Pro.local>
2017-11-21 22:09:33 +0000

    Displayed ShowCard on Show Landing Page

---
+++ client/src/shows/components/ShowLandingPage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ShowCard from './ShowCard';
+import '../../App.css';
+
+class ShowLandingPage extends Component {
+    state = {
+        popularShows: []
+    };
+
+    componentDidMount() {
+        fetch('/shows/popular')
+            .then(res => res.json())
+            .then(shows => this.setState({popularShows: shows}));
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        {this.state.popularShows.slice(0,4).map(show => (
+                            <div className="col-lg-3 text-center">
+                                <ShowCard
+                                    title={show.name}
+                                    posterPath={show.poster_path}
+                                    voteAverage={show.vote_average}
+                                    firstAirDate={show.first_air_date}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ShowLandingPage;
