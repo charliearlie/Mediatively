9727b613b699da5daf42d9dfe5262bd5cca75b3c
Charlie Waite <waitec03@mc-s115934.national.core.bbc.co.uk>
2017-11-24 15:45:18 +0000

    Updated config so others can use the config file with the API key still hidden

---
+++ config/config.js
@@ -0,0 +1,24 @@
+const API_KEY = require('./apiKey');
+
+config = {};
+config.tmdb = {
+    baseUrl: 'http://api.themoviedb.org/3/',
+    apiKey: API_KEY,
+    homepagePopular: 'discover/movie?sort_by=popularity.desc',
+    upcomingTitles: 'movie/upcoming',
+    tvPopular: 'tv/popular',
+    posterUrl: 'https://image.tmdb.org/t/p/',
+    imageSizes: {
+        posterSizes: {
+            tiny: 'w92',
+            extraSmall: 'w154',
+            small: 'w185',
+            medium: 'w342',
+            large: 'w500',
+            largest: 'w780',
+            original: 'original'
+        }
+    }
+};
+
+module.exports = config;
