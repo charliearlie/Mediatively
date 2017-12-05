2140069eaa7226766245b2247198f7a30af53d01
Charlie Waite <cw5790@gmail.com>
2017-12-05 23:26:20 +0000

    Made a cast member view and displayed on the movie page

--- client/src/globalComponents/CastMember.js
+++ client/src/globalComponents/CastMember.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const CastMember = (props) => {
-    const baseImageUrl = "https://image.tmdb.org/t/p/w300";
+    const baseImageUrl = "https://image.tmdb.org/t/p/w185";
     return (
         <div className="cast-member">
             <div className="row">
-                <div className="col-xs-4">
-                    <img className="cast-member-image" src={`${baseImageUrl + props.image}`} height="55px"/>
+                <div className="text-center">
+                    <img className="image-responsive" src={`${baseImageUrl + props.image}`} alt={props.name}/>
                 </div>
-                <div className="col-xs-8 cast-member-name">
+            </div>
+            <div className="row">
+                <div className="cast-member-name text-center">
                     <h4>{props.name}</h4>
                     <p>{props.character}</p>
                 </div>
             </div>
         </div>
     );
+}
+
+CastMember.propTypes = {
+    name: PropTypes.string.isRequired,
+    character: PropTypes.string.isRequired
 }
 
 export default CastMember;
--- client/src/movies/components/MoviePage.js
+++ client/src/movies/components/MoviePage.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import CoreInfo from '../../globalComponents/CoreInfo';
 import DocumentTitle from 'react-document-title';
+import CastMember from '../../globalComponents/CastMember';
 class MoviePage extends React.Component {
     
     render() {
         const { movieInfo } = this.props;
+        const title = movieInfo.title || 'Show Brainer';
         const releaseDate = movieInfo.release_date;
         return (
-            <DocumentTitle title={movieInfo.title}>
+            <DocumentTitle title={title}>
                 <div className="container">
-                    <CoreInfo 
-                        title={movieInfo.title}
-                        releaseDate={releaseDate}
-                        posterUrl={movieInfo.poster_path}
-                        inProduction={movieInfo.in_production}
-                        overview={movieInfo.overview}
-                        voteAverage={movieInfo.vote_average}
-                        genres={movieInfo.genres}
-                        cast={movieInfo.cast}
-                    />
-                    <hr />
+                    <div className="row">
+                        <CoreInfo
+                            title={movieInfo.title}
+                            releaseDate={releaseDate}
+                            posterUrl={movieInfo.poster_path}
+                            inProduction={movieInfo.in_production}
+                            overview={movieInfo.overview}
+                            voteAverage={movieInfo.vote_average}
+                            genres={movieInfo.genres}
+                        />
+                    </div>
+                    <hr data-content="Cast" />
+                    <div className="row cast-row">
+                        <div className="col-md-1" />
+                            {movieInfo.cast && movieInfo.cast.map(castMember => (
+                                <div key={`${castMember.id}_key`}>
+                                    <div className="col-xs-4 col-md-2">
+                                        <CastMember 
+                                            name={castMember.name}
+                                            character={castMember.character}
+                                            image={castMember.profile_path}
+                                        />
+                                    </div>
+                                </div>
+                            ))}
+                        <div className="col-md-1" />
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-12 text-center" style={{padding:'10px'}}>
+                            <a href="#">View full cast</a>
+                        </div>
+                    </div>
                 </div>
             </DocumentTitle>
         )
