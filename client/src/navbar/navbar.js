348261db688a8d4e9bb3b2f42edd49adcda0264a
Charlie Waite <cw5790@gmail.com>
2017-12-05 21:36:55 +0000

    Add search to navbar

--- client/src/navbar/navbar.js
+++ client/src/navbar/navbar.js
@@ -35,6 +35,16 @@ const NavBar = () => {
                             <Link to="/shows">Shows</Link>
                         </li>
                     </ul>
+                    <div class="col-sm-3 col-md-3">
+                        <form class="navbar-form" role="search">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Search" name="q" />
+                                <div class="input-group-btn">
+                                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                     <ul className="nav navbar-nav navbar-right">
                         <li>
                             <a href="../navbar/">Default</a>
@@ -48,11 +58,7 @@ const NavBar = () => {
                             </a>
                         </li>
                     </ul>
-                    
                 </div>
-                
-                
-
             </div>
         </nav>
     );
