f35759924aec862df5629e3d1774557410967550
Charlie Waite <cw5790@gmail.com>
2017-12-11 22:53:23 +0000

    Added CSS and a link to person row

--- client/src/people/components/PersonRow.js
+++ client/src/people/components/PersonRow.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import DefaultCastImage from '../../assets/default-cast-image.png';
+import { Link } from 'react-router-dom';
+import DefaultCastImage from '../../assets/default-cast-image-small.png';
 
 const PersonRow = (props) => {
     const baseImageUrl = "https://image.tmdb.org/t/p/w45";
     const image = props.image ? baseImageUrl + props.image : DefaultCastImage;
     return (
-        <div className="row person-row">
-            <div className="col-xs-2">
-                <img src={image} alt={`${props.name}_image`} />
+        <Link to={`/person/${props.id}`} >
+            <div className="row person-row">
+                <div className="col-xs-2">
+                    <img src={image} alt={`${props.name}_image`} />
+                </div>
+                <div className="col-xs-6">
+                    <h3>{props.name}</h3>
+                    <p>{props.character}</p>
+                </div>
             </div>
-            <div className="col-xs-6">
-                <h3>{props.name}</h3>
-                <p>{props.character}</p>
-            </div>
-        </div>
+        </Link>
     );
 }
 
