0c84b90d67232199801ca9bb1f3775449d0ad5c1
Charlie Waite <cw5790@gmail.com>
2017-12-11 22:54:47 +0000

    Change modal header text

--- client/src/people/components/CastModal.js
+++ client/src/people/components/CastModal.js
@@ -7,11 +7,13 @@ class CastModal extends React.Component {
         return (
             <Modal {...this.props} bsSize="large" aria-labelledby="contained-modal-title-lg">
                 <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-lg">Modal heading</Modal.Title>
+                    <Modal.Title id="contained-modal-title-lg">Cast</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     {this.props.cast.map(castMember => (
                         <PersonRow
+                            key={castMember.id}
+                            id={castMember.id}
                             name={castMember.name}
                             character={castMember.character}
                             image={castMember.profile_path}
