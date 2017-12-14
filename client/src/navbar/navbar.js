e45cf28fd1ce17bcf04c3815b95a8b12bdaf096b
Charlie Waite <cw5790@gmail.com>
2017-12-14 20:20:46 +0000

    Added dropdown to navbar

--- client/src/navbar/navbar.js
+++ client/src/navbar/navbar.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {
-    Link
-  } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
 
 const NavBar = () => {
     return (
@@ -46,9 +47,13 @@ const NavBar = () => {
                         </form>
                     </div>
                     <ul className="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="../navbar/">Default</a>
-                        </li>
+                    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                        <MenuItem eventKey={3.1}>Action</MenuItem>
+                        <MenuItem eventKey={3.2}>Another action</MenuItem>
+                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                    </NavDropdown>
                         <li>
                             <a href="../navbar-static-top/">Static top</a>
                         </li>
