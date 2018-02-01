270c6353345339e44ac21664c579ee862ead63ed
Charlie Waite <cw5790@gmail.com>
2018-02-01 11:54:27 +0000

    Replaced redux-thunk with redux-promise as the app middleware

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducers/index';
+import promise from 'redux-promise';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import rootReducer from '../reducers/index';
 
 export default function configureStore(initialState) {
-    return createStore(rootReducer, composeWithDevTools(
-        applyMiddleware(thunk)));
+    return createStore(rootReducer, composeWithDevTools(applyMiddleware(promise)));
 }
