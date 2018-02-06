8383dc19e4ec94aa2ad9948dde728c0e0170066a
Charlie Waite <cw5790@gmail.com>
2018-02-06 21:11:27 +0000

    Added prop types to suggested movies component

--- src/movies/components/SuggestedMoviesSection.js
+++ src/movies/components/SuggestedMoviesSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const SuggestedMoviesSection = (props) => {
 	const { suggestedMovies } = props;
@@ -21,6 +22,10 @@ const SuggestedMoviesSection = (props) => {
 			))}
 		</div>
 	);
+};
+
+SuggestedMoviesSection.propTypes = {
+	suggestedMovies: PropTypes.arrayOf().isRequired,
 };
 
 export default SuggestedMoviesSection;
