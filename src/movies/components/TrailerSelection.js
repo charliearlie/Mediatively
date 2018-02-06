1343d8ab05cc17ebfaee61e08dade93d3008da66
Charlie Waite <waitec03@mc-s115934.local>
2018-02-06 00:15:06 +0000

    Added prop types to Trailer Section

--- src/movies/components/TrailerSelection.js
+++ src/movies/components/TrailerSelection.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const TrailerSection = (props) => {
-	return (
-		<div className="MovieSelection">
-			<h2>Trailer</h2>
-			<iframe
-				className="MovieTrailer"
-				title={props.movieName}
-				src={`https://www.youtube.com/embed/${props.youtubeId}?rel=0&amp;showinfo=0`}
-				frameBorder="0"
-				allow="autoplay; encrypted-media"
-				allowFullScreen
-			/>
-			Row of other trailers here
-		</div>
-	);
+const TrailerSection = props => (
+	<div className="MovieSelection">
+		<h2>Trailer</h2>
+		<iframe
+			className="MovieTrailer"
+			title={props.movieName}
+			src={`https://www.youtube.com/embed/${props.youtubeId}?rel=0&amp;showinfo=0`}
+			frameBorder="0"
+			allow="autoplay; encrypted-media"
+			allowFullScreen
+		/>
+		Row of other trailers here
+	</div>
+);
+
+TrailerSection.propTypes = {
+	movieName: PropTypes.string.isRequired,
+	youtubeId: PropTypes.string,
+};
+
+TrailerSection.defaultProps = {
+	youtubeId: '',
 };
 
 export default TrailerSection;
