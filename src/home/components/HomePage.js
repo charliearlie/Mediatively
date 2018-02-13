05115baa4b0d316b1e96c1e1c291552abe83af79
Charlie Waite <cw5790@gmail.com>
2018-02-13 23:28:16 +0000

    Added some basic layout for the home page

--- src/home/components/HomePage.js
+++ src/home/components/HomePage.js
@@ -17,9 +17,16 @@ class HomePage extends Component {
 	}
 	render() {
 		return (
-			<div className="App">
-				<h1>Movies</h1>
-				{this.state.movies.map(movie => <div key={movie.id}>{movie.title}</div>)}
+			<div className="HomePage text-center">
+				<div className="col-12 card HomePage__banner">
+					<div className="HomePage__title-container">
+						<h1 className="HomePage__title">Mediatively</h1>
+					</div>
+				</div>
+				<div className="HomePage__content card">
+					<h1>Movies</h1>
+					{this.state.movies.map(movie => <div key={movie.id}>{movie.title}</div>)}
+				</div>
 			</div>
 		);
 	}
