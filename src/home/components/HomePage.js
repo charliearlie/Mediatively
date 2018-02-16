4940dfed41fd2566d8c9ae3697cb2313051468ed
Charlie Waite <cw5790@gmail.com>
2018-02-16 23:17:18 +0000

    Added a homepage collection component, along with some styling

--- src/home/components/HomePage.js
+++ src/home/components/HomePage.js
@@ -1,40 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import '../../App.css';
+import HomePageCollection from './HomePageCollection';
 
 
-class HomePage extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			movies: [],
-		};
-	}
-
-	componentDidMount() {
-		fetch('/movies/upcoming')
-			.then(res => res.json())
-			.then(movies => this.setState({ movies: movies.results }));
-	}
-	render() {
-		return (
-			<div className="HomePage text-center">
-				<div className="col-12 card HomePage__banner">
-					<div className="HomePage__title-container">
-						<h1 className="HomePage__title">Mediatively</h1>
-					</div>
-				</div>
-				<div className="HomePage__content card" style={{ height: '200px', textAlign: 'left' }}>
-					<div className="col-md-9">
-						<h2>Upcoming movies</h2>
-						<div className="col-md-4">
-							{console.log(this.state.movies)}
-						</div>
-					</div>
+const HomePage = (props) => {
+	const { boxOffice } = props;
+	return (
+		<div className="HomePage text-center">
+			<div className="col-12 card HomePage__banner">
+				<div className="HomePage__title-container">
+					<h1 className="HomePage__title">Mediatively</h1>
 				</div>
 			</div>
-		);
-	}
-}
+			<div className="col-md-12 card">
+				<div className="col-md-9 HomePage__content" style={{ textAlign: 'left', paddingLeft: '-16px' }}>
+					<h2>In Cinemas</h2>
+					<HomePageCollection type="movie" collection={boxOffice} />
+				</div>
+				<div className="col-md-3 HomePage__column" style={{ textAlign: 'left', paddingLeft: '-16px' }}>
+					<h2>Local</h2>
+					<ul>
+						<li>
+							<p>Everyman Muswell Hill</p>
+							<p>Fortis Green Road</p>
+						</li>
+						<li>
+							<p>Vue North Finchley</p>
+							<p>Great North Leisure Park</p>
+						</li>
+						<li>
+							<p>Crouch End Picturehouse</p>
+							<p>165 Tottenham Ln</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+HomePage.propTypes = {
+	boxOffice: PropTypes.arrayOf().isRequired,
+};
 
 export default HomePage;
---
+++ src/home/components/HomePageCollection.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import getRatingClass from '../../globalComponents/helpers/ratingsHelper';
+
+
+const HomePageCollection = props => (
+	<div>
+		{props.collection && props.collection.slice(0, 4).map(item => (
+			<Link to={`/movie/${item.id}`}>
+				<div className="col-xs-3 HomePage__collection">
+					<img
+						className="HomePage__collection-image image-responsive"
+						src={item.poster_path}
+						alt={item.title}
+					/>
+					<div className={`rating ${getRatingClass(item.vote_average)} collection-rating`}>
+						<p className="HomePage__collection-voteAverage">{item.vote_average}</p>
+					</div>
+				</div>
+			</Link>
+		))}
+	</div>
+);
+
+HomePageCollection.propTypes = {
+	collection: PropTypes.arrayOf().isRequired,
+};
+
+export default HomePageCollection;
