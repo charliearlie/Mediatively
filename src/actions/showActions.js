9f127611d4c85f0e79d7621681822318b9dbf8d0
Charlie Waite <cw5790@gmail.com>
2018-02-21 15:41:38 +0000

    It didn't work

--- src/actions/showActions.js
+++ src/actions/showActions.js
@@ -14,8 +14,7 @@ export function loadShowDetails(id) {
 	return {
 		type: LOAD_SHOW_DETAILS_SUCCESS,
 		payload: new Promise((resolve) => {
-			axios.get(`/shows/${id}`).then(response => resolve(response.data))
-			
+			axios.get(`/shows/${id}`).then(response => resolve(response.data));
 		}),
 	};
 }
