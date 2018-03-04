03c6020e464aa5d095a8d8dd44e914145e7e4e00
Charlie Waite <cw5790@gmail.com>
2018-03-04 21:18:34 +0000

    Added card to home page collection items

--- src/home/components/HomePageCollection.js
+++ src/home/components/HomePageCollection.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Card } from 'antd';
 import getRatingClass from '../../globalComponents/helpers/ratingsHelper';
 
 
 const HomePageCollection = (props) => {
+	const { Meta } = Card;
 	const type = props.type || '';
 	return (
 		<div>
 			{props.collection && props.collection.slice(0, 4).map(item => (
 				<Link to={`/${type}/${item.id}`}>
 					<div className="col-xs-3 HomePage__collection">
-						<img
-							className="HomePage__collection-image image-responsive"
-							src={item.poster_path}
-							alt={item.title}
-						/>
-						<div className={`rating ${getRatingClass(item.vote_average)} collection-rating`}>
-							<p className="HomePage__collection-voteAverage">{item.vote_average}</p>
-						</div>
+						<Card
+							hoverable
+							cover={<img alt={item.title} src={item.poster_path} />}
+						>
+							<Meta
+								title={item.title}
+								description={
+									<div className={`rating ${getRatingClass(item.vote_average)} collection-rating`}>
+										<p className="HomePage__collection-voteAverage">{item.vote_average}</p>
+									</div>
+								}
+							/>
+						</Card>
 					</div>
 				</Link>
 			))}
