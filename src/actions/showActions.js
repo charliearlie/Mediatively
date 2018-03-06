869dd7f84e142c9512a80ef4147bbdc660036a04
Charlie Waite <cw5790@gmail.com>
2018-03-06 14:54:08 +0000

    Added tonight's shows to the homepeage

--- src/actions/showActions.js
+++ src/actions/showActions.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
-import { LOAD_POPULAR_SHOWS_SUCCESS, LOAD_SHOW_DETAILS_SUCCESS } from './actionTypes';
+import { LOAD_POPULAR_SHOWS_SUCCESS, LOAD_SHOW_DETAILS_SUCCESS, LOAD_TODAYS_SHOWS_SUCCESS } from './actionTypes';
 
 export function loadPopularShows() {
 	return {
 		type: LOAD_POPULAR_SHOWS_SUCCESS,
 		payload: new Promise((resolve) => {
 			axios.get('/shows/popular').then(response => resolve(response.data));
+		}),
+	};
+}
+
+export function loadTodaysShows() {
+	return {
+		type: LOAD_TODAYS_SHOWS_SUCCESS,
+		payload: new Promise((resolve) => {
+			axios.get('/shows/today').then(response => resolve(response.data));
 		}),
 	};
 }
