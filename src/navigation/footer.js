c7215ddf58b57e9f0078c46c5e5f168b53a72185
Charlie Waite <cw5790@gmail.com>
2018-03-06 23:22:17 +0000

    Added padding between body and footer

--- src/navigation/footer.js
+++ src/navigation/footer.js
@@ -8,7 +8,7 @@ const Footer = () => (
 			<a href="https://twitter.com/charliecw90"><i className="fab fa-twitter fa-3x fa-fw" /></a>
 			<a href="https://www.linkedin.com/in/charlie-waite"><i className="fab fa-linkedin-in fa-3x fa-fw" /></a>
 			<a href="https://www.github.com/charliearlie"><i className="fab fa-github fa-3x fa-fw" /></a>
-			<a href="#"><i className="fab fa-youtube fa-3x fa-fw" /></a>
+			<a href="https://www.youtube.com"><i className="fab fa-youtube fa-3x fa-fw" /></a>
 		</div>
 	</div>
 );
