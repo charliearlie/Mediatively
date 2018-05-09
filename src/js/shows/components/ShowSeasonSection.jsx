4b3379dee99ac530501476d471a13bcc20787976
Charlie Waite <cw5790@gmail.com>
2018-05-09 21:56:39 +0100

    Added a show season section (WIP)

---
+++ src/js/shows/components/ShowSeasonSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from '../../globalComponents/Card';
+
+const ShowSeasonSection = ({ seasons }) => (
+	<Card
+		styles={{ minHeight: '400px' }}
+		shadowLevel={2}
+	>
+		{seasons.map(season => (
+			JSON.stringify(season)
+		))}
+	</Card>
+);
+
+ShowSeasonSection.propTypes = {
+	seasons: PropTypes.arrayOf().isRequired,
+};
+
+export default ShowSeasonSection;
