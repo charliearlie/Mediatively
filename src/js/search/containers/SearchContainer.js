c1e4caf54c202c3a41b72fe178d2dee29bb98760
Charlie Waite <cw5790@gmail.com>
2018-05-16 23:30:41 +0100

    Updated webpack config for production

--- src/js/search/containers/SearchContainer.js
+++ src/js/search/containers/SearchContainer.js
@@ -1,26 +1,18 @@
 import { connect } from 'react-redux';
 import Search from '../components/Search';
 import ApiHoc from '../../globalComponents/ApiHoc';
-import { searchActions } from '../../actions/searchActions';
+import { performSearch } from '../../actions/searchActions';
 
 const mapStateToProps = state => ({
 	results: state.search.results,
 	numberOfResults: state.search.numberOfResults,
 	totalPages: state.search.totalPages,
-	currentPage: state.search.page
+	currentPage: state.search.page,
 });
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-	const query = ownProps.match ? ownProps.match.params.query : undefined;
-	const {changeFilter, updateSearch, jumpToPage, resetSearch, movePage} = searchActions;
-	return {
-		performSearch: () => dispatch(performSearch(query)),
-		onFilterChange: type => dispatch(changeFilter(filter)),
-		onPageChange: pageNumber => dispatch(updateSearch(jumpToPage(pageNumber))),
-		resetSearch: () => dispatch(resetSearch()),
-		onNextPage: () => dispatch(updateSearch(movePage(+1))),
-		onPreviousPage: () => dispatch(updateSearch(movePage(-1)))
-	};
-};
+const mapDispatchToProps = dispatch => ({
+	performSearch: query => dispatch(performSearch(query)),
+});
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(ApiHoc(Search));
